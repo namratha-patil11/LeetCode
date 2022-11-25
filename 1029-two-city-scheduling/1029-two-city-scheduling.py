a542b0b0b95127a9388ef352cf8d8dadd90b7993
Namratha Patil <101965740+namratha-patil11@users.noreply.github.com>
2022-11-25 16:49:10 -0600

    Time: 38 ms (97.24%), Space: 13.8 MB (77.66%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,14 +1,14 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        #using greedy Approach
-        # time complexity is O(n log n) - for sort 
-        # space Complexity is 
+        
+        length = len(costs)
+        mid = length//2
         res = 0
-        n = len(costs) // 2
         
-        costs.sort(key = lambda x : x[0] - x[1])
+        costs.sort(key = lambda x  : x[0] - x[1])
+        
+        for i in range(mid):
+            res = res + costs[i][0] + costs[i+mid][1]
 
-        for i in range(n):
-            res += costs[i][0] + costs[i + n][1]
-        
         return res
+            
