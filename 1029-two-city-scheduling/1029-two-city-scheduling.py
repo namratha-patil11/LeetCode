c6597ddce1d2f9fdcaf2eb0809cc490bd0972762
Namratha Patil <101965740+namratha-patil11@users.noreply.github.com>
2022-11-29 16:15:10 -0600

    Time: 40 ms (95.62%), Space: 13.8 MB (77.65%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -5,10 +5,10 @@ class Solution:
         mid = length//2
         res = 0
         
-        costs.sort(key = lambda x  : x[0] - x[1])
+        costs.sort(key = lambda x: x[0] - x[1])
         
         for i in range(mid):
             res = res + costs[i][0] + costs[i+mid][1]
-
+        
         return res
-            
+        
